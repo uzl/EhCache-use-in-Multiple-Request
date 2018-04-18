2159f670d1517ed1fcde163cbef9c58893dd7044
Uzzal Podder <uzzwalbd@gmail.com>
2018-04-18 11:46:19 +0600

    Generated by Spring Initializer
    Maven Basic Project Structure is initialized.

---
+++ src/main/java/io/github/uzl/EhCacheuseinMultipleRequest/EhCacheUseInMultipleRequestApplication.java
@@ -0,0 +1,12 @@
+package io.github.uzl.EhCacheuseinMultipleRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhCacheUseInMultipleRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhCacheUseInMultipleRequestApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/uzl/EhCacheuseinMultipleRequest/EhCacheUseInMultipleRequestApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.uzl.EhCacheuseinMultipleRequest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EhCacheUseInMultipleRequestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
